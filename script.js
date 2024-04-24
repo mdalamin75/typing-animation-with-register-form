d4c58d59428d48c2e2f84bbcfbdfb230f0a69364
MD.AL-AMIN <mdalmain67378@gmail.com>
2024-04-24 16:21:00 +0600

    upload on github

---
+++ script.js
@@ -0,0 +1,26 @@
+const text1 = "Welcome to our sign-up page.";
+const text2 = "Let's get started!";
+const delay = 100; // Delay between typing each character
+
+let index1 = 0;
+let index2 = 0;
+
+function type() {
+  const textSpan1 = document.getElementById('text1');
+  const textSpan2 = document.getElementById('text2');
+  
+  if (index1 < text1.length) {
+    textSpan1.textContent += text1[index1];
+    index1++;
+    setTimeout(type, delay);
+  } else if (index2 < text2.length) {
+    textSpan2.textContent += text2[index2];
+    index2++;
+    setTimeout(type, delay);
+  } else {
+    const inputContainer = document.getElementById('input-container');
+    inputContainer.style.display = 'block';
+  }
+}
+
+type();
